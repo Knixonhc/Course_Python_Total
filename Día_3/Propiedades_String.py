70f77c8fa7e1341283fb1c0b27676abd6636967d
Knixonhc <knixonhc@gmail.com>
2024-04-21 22:46:07 -0500

    Dia 3

---
+++ Día_3/Propiedades_String.py
@@ -0,0 +1,21 @@
+# Práctica Propiedades de Strings 1
+# Concatena 15 veces el texto "Repetición" y muestra el resultado en pantalla. Por suerte, conoces que los strings son multiplicables y puedes realizar esta actividad de forma simple y elegante.
+texto = "Repetición"
+print(texto*15)
+
+
+# Práctica Propiedades d/e Strings 2
+# Verifica si la palabra "agua" no se encuentra en el siguiente haiku. Debes imprimir el booleano.
+#
+# Tierra mojada,
+# mis recuerdos de viaje
+# entre las lluvias
+texto2 = """Tierra mojada,
+mis recuerdos de viaje
+entre las lluvias"""
+print("agua" not in texto2)
+
+# Práctica Propiedades de Strings 3
+# Muestra en pantalla el largo (en números de caracteres) de la palabra electroencefalografista.
+texto3 = "electroencefalografista"
+print(len(texto3))
